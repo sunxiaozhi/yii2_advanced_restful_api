62a4cd4aab2032b791408578e8ad43541db57898
sunxiaozhi <1129535445@qq.com>
2018-12-13 16:48:28 +0800

    update behaviors

--- api/modules/v1/controllers/GoodsController.php
+++ api/modules/v1/controllers/GoodsController.php
@@ -7,6 +7,7 @@ use yii\web\Response;
 use yii\filters\auth\CompositeAuth;
 use yii\filters\auth\QueryParamAuth;
 use yii\filters\RateLimiter;
+use yii\helpers\ArrayHelper;
 
 class GoodsController extends ActiveController
 {
@@ -14,7 +15,7 @@ class GoodsController extends ActiveController
 
     public function behaviors()
     {
-        $behaviors = parent::behaviors();
+        /*$behaviors = parent::behaviors();
 
         //速率限制
         $behaviors['rateLimiter'] = [
@@ -26,7 +27,7 @@ class GoodsController extends ActiveController
         $behaviors['authenticator'] = [
             'class' => CompositeAuth::className(),
             'authMethods' => [
-                /*下面是三种验证access_token方式*/
+                //下面是三种验证access_token方式
                 //1.HTTP 基本认证: access token 当作用户名发送，应用在access token可安全存在API使用端的场景，例如，API使用端是运行在一台服务器上的程序。
                 //HttpBasicAuth::className(),
                 //2.OAuth 2: 使用者从认证服务器上获取基于OAuth2协议的access token，然后通过 HTTP Bearer Tokens 发送到API 服务器。
@@ -37,7 +38,37 @@ class GoodsController extends ActiveController
             ],
         ];
 
+        //格式化应答数据
         $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
-        return $behaviors;
+
+        return $behaviors;*/
+
+        return ArrayHelper::merge(parent::behaviors(),[
+            //速率限制
+            'rateLimiter' => [
+                'enableRateLimitHeaders' => true,
+            ],
+
+            //授权认证
+            'authenticator' => [
+                'authMethods' => [
+                    //下面是三种验证access_token方式
+                    //1.HTTP 基本认证: access token 当作用户名发送，应用在access token可安全存在API使用端的场景，例如，API使用端是运行在一台服务器上的程序。
+                    //HttpBasicAuth::className(),
+                    //2.OAuth 2: 使用者从认证服务器上获取基于OAuth2协议的access token，然后通过 HTTP Bearer Tokens 发送到API 服务器。
+                    //HttpBearerAuth::className(),
+                    //3.请求参数: access token 当作API URL请求参数发送，这种方式应主要用于JSONP请求，因为它不能使用HTTP头来发送access token
+                    //http://localhost/user/index/index?access-token=123
+                    QueryParamAuth::className(),
+                ]
+            ],
+
+            //格式化应答数据
+            'contentNegotiator' => [
+                'formats' => [
+                    'text/html' => Response::FORMAT_JSON
+                ]
+            ]
+        ]);
     }
 }
